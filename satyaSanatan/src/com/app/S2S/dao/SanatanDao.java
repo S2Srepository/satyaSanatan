fd88230060be3009b60da3c688c54e28cd4ebd2b
Amit <Amit@DESKTOP-8VTM8R5>
2018-10-25 00:53:50 +0530

    commit by Amit

--- satyaSanatan/src/com/app/S2S/dao/SanatanDao.java
+++ satyaSanatan/src/com/app/S2S/dao/SanatanDao.java
@@ -3,9 +3,18 @@ package com.app.S2S.dao;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.app.S2S.beans.ContactUs;
 @Repository
-public class SanatanDao {
+public class SanatanDao implements SanatanDaoInteface {
 	@Autowired
 	private SessionFactory sessionFactory;
+
+	@Override
+	public void saveContact(ContactUs contact) {
+		
+sessionFactory.getCurrentSession().saveOrUpdate(contact);
+		
+	}
    
 }
--- satyaSanatan/src/com/app/S2S/service/UserDataValue.java
+++ satyaSanatan/src/com/app/S2S/service/UserDataValue.java
@@ -4,11 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.app.S2S.beans.ContactUs;
 import com.app.S2S.dao.SanatanDao;
 
 @Service
 @Transactional
-public class UserDataValue {
+public class UserDataValue implements UserData {
 @Autowired
 SanatanDao dao;
+
+@Override
+public void saveContact(ContactUs contact){
+	
+	dao.saveContact(contact);
+	
+	
+}
 }
